077615922dea29ac242cf8b6b4ff3a9460707e1d
itishnic <122455719+itishnic@users.noreply.github.com>
2024-07-08 19:01:07 +0300

    commit

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -8,7 +8,8 @@ import { getMoviesCast } from "../../api/movies-api";
 const MovieCast = () => {
   const { movieId } = useParams();
 
-  const defaultImg = "path/to/placeholder.svg"; 
+  const defaultImg =
+    "<https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg>"; 
 
   const [cast, setCast] = useState([]);
   const [isloading, setIsLoading] = useState(false);
